6416329647896c35bc2612708a9f89149d8e00ed
Usama Ahamd <sohiusama1@gmail.com>
2024-06-06 13:10:33 +0500

    1234567890

--- lib/app/notifications/notifications_view.dart
+++ lib/app/notifications/notifications_view.dart
@@ -1,22 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:find_me/app/auth/components/auth_appbar.dart';
 import 'package:find_me/app/notifications/notifications_controller.dart';
 import 'package:find_me/components/appbars/topbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
-class NotificationsView extends StatefulWidget {
+class NotificationsView extends StatelessWidget {
   const NotificationsView({super.key});
 
-  @override
-  State<NotificationsView> createState() => _NotificationsViewState();
-}
-
-class _NotificationsViewState extends State<NotificationsView> {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<NotificationsController>(
       builder: (controller) => Scaffold(
-        appBar: topBar(name: 'Sign Up'),
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          forceMaterialTransparency: true,
+          title: topBar(name: 'Notifications'),
+        ),
+        body: SafeArea(
+            child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50.w),
+            child: Column(
+              children: [
+                // Container(
+                //     decoration: BoxDecoration(border: Border.all()),
+                //     child: topBar(name: 'Notifications')),
+                Row(
+                  children: [Text('data')],
+                )
+              ],
+            ),
+          ),
+        )),
       ),
     );
   }
