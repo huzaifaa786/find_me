50ddbc60fea47f6abc9cdf657aa9821eff2c314f
Hafiz Bismil <hafizbismil@gmail.com>
2024-06-07 10:50:30 +0500

    hafiz bismil

--- lib/app/account/change_phonenumber/change_phonenumber_binding.dart
+++ lib/app/account/change_phonenumber/change_phonenumber_binding.dart
@@ -1,5 +1,5 @@
 import 'package:find_me/app/account/change_phonenumber/change_phonenumber_controller.dart';
-import 'package:find_me/app/account/profile/account_controller.dart';
+
 import 'package:get/get.dart';
 
 class ChangePhoneNumberBinding extends Bindings {
