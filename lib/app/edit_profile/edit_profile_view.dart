f0bc0e4eeb8e7720bd33a8554100626fdd9b8476
Panda <mabdullahbhatti9193@gmail.com>
2024-06-11 16:48:43 +0500

    edit_profile

--- lib/app/edit_profile/edit_profile_view.dart
+++ lib/app/edit_profile/edit_profile_view.dart
@@ -59,7 +59,8 @@ class EditProfileView extends StatelessWidget {
                         child: Row(
                           children: [
                             Checkbox(
-                              shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(1)),
+                              shape: BeveledRectangleBorder(
+                                  borderRadius: BorderRadius.circular(1)),
                               side: BorderSide(
                                 color: AppColors.primary_color,
                                 // width: 1,
@@ -118,7 +119,8 @@ class EditProfileView extends StatelessWidget {
                     ),
                   ],
                 ),
-               Row(
+                Gap(16.w),
+                Row(
                   children: [
                     Gap(36.w),
                     GestureDetector(
@@ -135,7 +137,8 @@ class EditProfileView extends StatelessWidget {
                         child: Row(
                           children: [
                             Checkbox(
-                              shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(1)),
+                              shape: BeveledRectangleBorder(
+                                  borderRadius: BorderRadius.circular(1)),
                               side: BorderSide(
                                 color: AppColors.primary_color,
                                 // width: 1,
@@ -180,6 +183,85 @@ class EditProfileView extends StatelessWidget {
                               fontWeight: FontWeight.w500,
                             ),
                             Gap(10.h),
+                            // SvgPicture.asset('assets/icons/verified.svg'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Gap(8.w),
+                    AppText(
+                      title: 'Edit',
+                      size: 11,
+                      color: AppColors.primary_color,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ],
+                ),
+                Gap(16.w),
+                
+                Row(
+                  children: [
+                    Gap(36.w),
+                    GestureDetector(
+                      onTap: () {
+                        controller.toggleDefault(!controller.isDefault);
+                      },
+                      child: Container(
+                        height: 68.h,
+                        width: 290.w,
+                        decoration: BoxDecoration(
+                          color: AppColors.border_grey,
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: Row(
+                          children: [
+                            Checkbox(
+                              shape: BeveledRectangleBorder(
+                                  borderRadius: BorderRadius.circular(1)),
+                              side: BorderSide(
+                                color: AppColors.primary_color,
+                                // width: 1,
+                              ),
+                              value: controller.isDefault,
+                              onChanged: (value) {
+                                controller.toggleDefault(value ?? false);
+                              },
+                            ),
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                // color: Colors.blue,
+                                borderRadius: BorderRadius.circular(80.r),
+                                border: Border.all(
+                                  color:
+                                      AppColors.primary_color, // Border color
+                                  width: 1, // Border width
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: AppColors.shadow_black
+                                        .withOpacity(0.25),
+                                    spreadRadius: 1,
+                                    blurRadius: 10.7,
+                                    offset: Offset(0, 0),
+                                  ),
+                                ],
+                                image: DecorationImage(
+                                  image: CachedNetworkImageProvider(
+                                    controller.selectedItem.avatarUrl,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Gap(10.h),
+                            AppText(
+                              title: 'Almarwan general regss',
+                              size: 10,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            Gap(10.h),
                             SvgPicture.asset('assets/icons/verified.svg'),
                           ],
                         ),
@@ -194,8 +276,84 @@ class EditProfileView extends StatelessWidget {
                     ),
                   ],
                 ),
-             
-             
+                Gap(16.w),
+                Row(
+                  children: [
+                    Gap(36.w),
+                    GestureDetector(
+                      onTap: () {
+                        controller.toggleDefault(!controller.isDefault);
+                      },
+                      child: Container(
+                        height: 68.h,
+                        width: 290.w,
+                        decoration: BoxDecoration(
+                          color: AppColors.border_grey,
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: Row(
+                          children: [
+                            Checkbox(
+                              shape: BeveledRectangleBorder(
+                                  borderRadius: BorderRadius.circular(1)),
+                              side: BorderSide(
+                                color: AppColors.primary_color,
+                                // width: 1,
+                              ),
+                              value: controller.isDefault,
+                              onChanged: (value) {
+                                controller.toggleDefault(value ?? false);
+                              },
+                            ),
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                // color: Colors.blue,
+                                borderRadius: BorderRadius.circular(80.r),
+                                border: Border.all(
+                                  color:
+                                      AppColors.primary_color, // Border color
+                                  width: 1, // Border width
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: AppColors.shadow_black
+                                        .withOpacity(0.25),
+                                    spreadRadius: 1,
+                                    blurRadius: 10.7,
+                                    offset: Offset(0, 0),
+                                  ),
+                                ],
+                                image: DecorationImage(
+                                  image: CachedNetworkImageProvider(
+                                    controller.selectedItem.avatarUrl,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Gap(10.h),
+                            AppText(
+                              title: 'Almarwan general regss',
+                              size: 10,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            Gap(10.h),
+                            // SvgPicture.asset('assets/icons/verified.svg'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Gap(8.w),
+                    AppText(
+                      title: 'Edit',
+                      size: 11,
+                      color: AppColors.primary_color,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ],
+                ),
               ],
             );
           },
