8dcadcbfe9a1d5112f1f3510b3acf66d0eb1355d
Panda <mabdullahbhatti9193@gmail.com>
2024-06-11 17:57:13 +0500

    changes

--- lib/app/splash/splash_controller.dart
+++ lib/app/splash/splash_controller.dart
@@ -22,7 +22,7 @@ class SplashController extends GetxController {
 
     if (apiToken != '') {
     } else {
-      Get.toNamed(AppRoutes.policies);
+      Get.toNamed(AppRoutes.intro);
     }
   }
 }
