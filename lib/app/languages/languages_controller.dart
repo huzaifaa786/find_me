aeaca587832ae43f4d7a60dd6d973f3c90220917
Panda <mabdullahbhatti9193@gmail.com>
2024-06-11 14:40:40 +0500

    languages

--- lib/app/languages/languages_controller.dart
+++ lib/app/languages/languages_controller.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:find_me/utils/colors/app_colors.dart';
 
 class LanguagesController extends GetxController {
   static LanguagesController get instance => Get.find();
@@ -7,6 +9,11 @@ class LanguagesController extends GetxController {
 
   void changeLanguage(int index) {
     selectedLanguage = index;
-    update(); // Notify listeners to rebuild the UI
+    update();
+  }
+
+  // Method to get the color based on the selection state
+  Color getRadioColor(int value) {
+    return value == selectedLanguage ? AppColors.primary_color : AppColors.primary_color;
   }
 }
