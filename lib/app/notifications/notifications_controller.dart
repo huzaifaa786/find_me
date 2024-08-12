310d2612ac5ed9509467ec1e266f0178cca606d3
Panda <mabdullahbhatti9193@gmail.com>
2024-08-12 14:41:21 +0500

    12345

--- lib/app/notifications/notifications_controller.dart
+++ lib/app/notifications/notifications_controller.dart
@@ -1,14 +1,14 @@
-
-
 import 'package:find_me/api/notifications/get_notifications_api.dart';
+import 'package:find_me/models/notification_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'dart:developer';
 
-
 class NotificationsController extends GetxController {
   static NotificationsController instanse = Get.find();
   final bool status = false;
+  NotificationModel? notification;
 
   @override
   void onInit() {
@@ -16,12 +16,16 @@ class NotificationsController extends GetxController {
     super.onInit();
     notifications();
   }
+  List<NotificationModel> notificationss = [];
+
 
   notifications() async {
     var response = await GetNotificationsApi.getNotifications();
-     log('$response'); 
+    log('$response');
     if (response.isNotEmpty) {
+      notification = NotificationModel.fromjson(response['notifications']);
       GetStorage box = GetStorage();
+      update();
       box.read("api_token");
     }
   }
